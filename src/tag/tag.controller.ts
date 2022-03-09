3dfc11cbbaa268070e9a1365b4cd3cdb90ef3ba5
AndreykaMen <aodinmg@gmail.com>
2022-03-10 01:50:31 +0600

    Создал настройку для миграции базы данных , добавил команды удаление всех таблиц и создание новой таблицы с созданием файла в src/migrations

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -6,9 +6,10 @@ import {TagService} from "@app/tag/tag.service";
 export class TagController {
     constructor(private readonly tagService: TagService) {
     }
+
     @Get()
-    async findAll(): Promise<{tags: string[]}> {
-        const tags =  await this.tagService.findAll();
+    async findAll(): Promise<{ tags: string[] }> {
+        const tags = await this.tagService.findAll();
         return {
             tags: tags.map((tag) => tag.name),
         }
