663c2db6160d819cc91b9cf02fdd806c614242f6
AndreykaMen <aodinmg@gmail.com>
2022-03-10 14:26:11 +0600

    Создал кофиг с переменными. Добавил новую переменную , для генератора токена, ключ-декодер и передал его в функцию генератор токена generateJwt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,8 @@ import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserEntity } from '@app/user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
 
 @Injectable()
 export class UserService {
@@ -14,7 +16,26 @@ export class UserService {
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
-    console.log('newUser', newUser);
     return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): any {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
