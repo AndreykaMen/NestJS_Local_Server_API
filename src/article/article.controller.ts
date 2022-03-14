fd4d8367417cdcb7382004fee6e7d866e599abed
AndreykaMen <aodinmg@gmail.com>
2022-03-14 21:58:45 +0600

    Установил декоратор для котролена article @UseGuard, для защиты роутов. Только зарегесрированный пользователь сможет пройти по роуту

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from '@app/article/article.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async create() {
     return this.articleService.createArticle();
   }
