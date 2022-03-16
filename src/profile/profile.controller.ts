7e276abdd687bcee866248319f2174e5a104e1c7
AndreykaMen <aodinmg@gmail.com>
2022-03-17 02:03:16 +0600

    Создал новый сервис profile, получаю профиль пользователя по ссылке

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from '@app/user/decorators/user.decorator';
+import { ProfileResponseInterface } from '@app/profile/types/profileResponse.interface';
+import { ProfileService } from '@app/profile/profile.service';
+
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') ProfileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      ProfileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+}
